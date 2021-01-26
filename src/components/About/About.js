31f691358800622643f3d63cb101b21b71f56d99
Lucy Quinn <lucy.quinn.uk@gmail.com>
2021-01-26 17:54:22 +0100

    change profile image.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Lucy from './../../images/lucy.jpg';
+import Lucy from './../../images/lucy.png';
 import './about.css';
 
 const About = () => {
     return (
         <div id="about">
             <img className="lucy" src={Lucy} alt="portrait of Lucy" />
-            <p>&lt;div&gt;</p>
             <h2>About Lucy</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut explicabo quasi consequuntur, eos recusandae magnam, temporibus itaque illum quo quibusdam fugit nobis quos animi, ut dolorem perspiciatis suscipit aliquid atque?</p>
         </div>
