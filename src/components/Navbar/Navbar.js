125d4d7640897c085564886fcd5ff33e22e0477f
Lucy Quinn <lucy.quinn.uk@gmail.com>
2021-01-27 19:40:59 +0100

    add dark, light mode functionality to navbar and apply correct styling.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,30 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
 import ToggleButtonOpen from './../../images/button-open.svg';
 import ToggleButtonClose from './../../images/button-close.svg';
-import LightBulb from './../../images/button-light-bulb.svg';
 import Logo from './../../images/logo.svg';
-
+import ThemeToggle from './../../components/ThemeToggle/ThemeToggle';
+import { ThemeContext } from '../../contexts/ThemeContext';
 import './Navbar.css';
 
 const Navbar = () => {
     const [isActive, setActive] = useState(false);
+    const { isLightTheme, themes } = useContext(ThemeContext);
+    const theme = isLightTheme ? themes.light : themes.dark;
     const handleToggle = () => {
         setActive(!isActive);
     }
     return (
         <div>
             <nav className="navbar">
-
                 {isActive ?
                     (<a href="#" className="toggle-button cross-toggle" onClick={handleToggle}>
                         <img src={ToggleButtonClose} alt="toggle button close icon" />
                     </a>) :
                     (<div>
                         <div className="bulb-and-logo">
-                            <div>
-                                <img src={LightBulb} alt="icon of light bulb" />
-                            </div>
+                            <ThemeToggle />
                             <div className="brand-title">
                                 <img src={Logo} alt="logo icon" />
                             </div>
@@ -38,22 +37,22 @@ const Navbar = () => {
                 <div className="navbar-links">
                     <ul className={isActive ? "active" : null}>
                         <li>
-                            <p>Menu</p>
+                            <p style={{ color: theme.div }}>Menu</p>
                         </li>
                         <Link to="about">
-                            <h2>About Lucy</h2>
+                            <h2 style={{ color: theme.primaryColor }}>About Lucy</h2>
                         </Link>
                         <Link to="portfolio">
-                            <h2>Portfolio</h2>
+                            <h2 style={{ color: theme.primaryColor }}>Portfolio</h2>
                         </Link>
                         <Link to="education">
-                            <h2>Education</h2>
+                            <h2 style={{ color: theme.primaryColor }}>Education</h2>
                         </Link>
                         <Link to="contact">
-                            <h2>Contact</h2>
+                            <h2 style={{ color: theme.primaryColor }}>Contact</h2>
                         </Link>
                         <Link to="#">
-                            <h2>Resume</h2>
+                            <h2 style={{ color: theme.primaryColor }}>Resume</h2>
                         </Link>
                     </ul>
                 </div>
