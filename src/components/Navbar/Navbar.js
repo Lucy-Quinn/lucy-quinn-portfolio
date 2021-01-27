1bbf07eac5ea4c44718a51afd5b6ad74bf604f11
Lucy Quinn <lucy.quinn.uk@gmail.com>
2021-01-27 17:39:42 +0100

    add close toggle button svg and css styling.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
-import ToggleButton from './../../images/button-hamburger.svg';
+import ToggleButtonOpen from './../../images/button-open.svg';
+import ToggleButtonClose from './../../images/button-close.svg';
 import LightBulb from './../../images/button-light-bulb.svg';
 import Logo from './../../images/logo.svg';
 
@@ -14,29 +15,46 @@ const Navbar = () => {
     return (
         <div>
             <nav className="navbar">
-                <div className="bulb-and-logo">
-                    <div>
-                        <img src={LightBulb} alt="icon of light bulb" />
-                    </div>
-                    <div className="brand-title">
-                        <img src={Logo} alt="logo icon" />
-                    </div>
-                </div>
+
                 {isActive ?
                     (<a href="#" className="toggle-button cross-toggle" onClick={handleToggle}>
-                        <i className="fas fa-times"></i>
+                        <img src={ToggleButtonClose} alt="toggle button close icon" />
                     </a>) :
-                    (<a href="#" className="toggle-button" onClick={handleToggle}>
-                        <img src={ToggleButton} alt="toggle button icon" />
-                    </a>)
+                    (<div>
+                        <div className="bulb-and-logo">
+                            <div>
+                                <img src={LightBulb} alt="icon of light bulb" />
+                            </div>
+                            <div className="brand-title">
+                                <img src={Logo} alt="logo icon" />
+                            </div>
+                        </div>
+
+                        <a href="#" className="toggle-button" onClick={handleToggle}>
+                            <img src={ToggleButtonOpen} alt="toggle button open icon" />
+                        </a>
+                    </div>)
                 }
                 <div className="navbar-links">
                     <ul className={isActive ? "active" : null}>
-                        <Link to="about">About Lucy</Link>
-                        <Link to="portfolio">Portfolio</Link>
-                        <Link to="education">Education</Link>
-                        <Link to="contact">Contact</Link>
-                        <Link to="#">Resume</Link>
+                        <li>
+                            <p>Menu</p>
+                        </li>
+                        <Link to="about">
+                            <h2>About Lucy</h2>
+                        </Link>
+                        <Link to="portfolio">
+                            <h2>Portfolio</h2>
+                        </Link>
+                        <Link to="education">
+                            <h2>Education</h2>
+                        </Link>
+                        <Link to="contact">
+                            <h2>Contact</h2>
+                        </Link>
+                        <Link to="#">
+                            <h2>Resume</h2>
+                        </Link>
                     </ul>
                 </div>
             </nav>
