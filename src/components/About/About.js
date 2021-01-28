0b32880af85ae81335861fceecd07aef5a74a0b5
Lucy Quinn <lucy.quinn.uk@gmail.com>
2021-01-28 14:03:41 +0100

    add styled components and functionality for dark and light theme.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../../contexts/ThemeContext';
 import Lucy from './../../images/lucy.png';
-import './about.css';
+import styled from 'styled-components';
+// import './about.css';
+
+//Styled components
+
+const Container = styled.div`
+    padding: 5px;
+  background: ${({ isLightTheme, theme }) => isLightTheme ? null : theme.background};
+
+`
+
+const Image = styled.img`
+height: 200px;
+  width: auto;
+  border-radius: 50%;
+`
+
+const Div = styled.p`
+  color: ${({ isLightTheme, theme }) => isLightTheme ? theme.primaryColor : theme.div};
+  font-weight: 300;
+  margin: 0;
+`
+
+const Heading = styled.h2`
+  color: ${({ theme }) => theme.primaryColor};
+`
+
+const Description = styled.p`
+  color: ${({ theme }) => theme.bodyCopy};
+  margin-bottom: 100px;
+`
+
 
 const About = () => {
+    const { isLightTheme, themes } = useContext(ThemeContext);
+    const theme = isLightTheme ? themes.light : themes.dark;
     return (
-        <div id="about">
-            <img className="lucy" src={Lucy} alt="portrait of Lucy" />
-            <h2>About Lucy</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut explicabo quasi consequuntur, eos recusandae magnam, temporibus itaque illum quo quibusdam fugit nobis quos animi, ut dolorem perspiciatis suscipit aliquid atque?</p>
-        </div>
+        <Container isLightTheme={isLightTheme} theme={theme} id="about">
+            <Image className="lucy" src={Lucy} alt="portrait of Lucy" ></Image>
+            <Div isLightTheme={isLightTheme} theme={theme}>&lt;div&gt;</Div>
+            <Heading theme={theme}>About Lucy</Heading>
+            <Description theme={theme}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut explicabo quasi consequuntur, eos recusandae magnam, temporibus itaque illum quo quibusdam fugit nobis quos animi, ut dolorem perspiciatis suscipit aliquid atque?</Description>
+        </Container>
     );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,7 +27,6 @@ const LanguageTabs = styled.div`
 `
 const Div = styled.p`
   color: ${({ isLightTheme, theme }) => isLightTheme ? theme.primaryColor : theme.div};
-  font-weight: 300;
   margin: 0;
 `
 const Lucy = styled.h3`
@@ -41,41 +40,40 @@ const Title = styled.h1`
 
 const LanguageList = styled.p`
   color: ${({ theme }) => theme.primaryColor};
-  font-weight: 300;
 `
 
 const Header = () => {
-    const { isLightTheme, themes } = useContext(ThemeContext);
-    const theme = isLightTheme ? themes.light : themes.dark;
-    return (
-        <header style={{
-            padding: '10px',
-            background: `linear-gradient(
+  const { isLightTheme, themes } = useContext(ThemeContext);
+  const theme = isLightTheme ? themes.light : themes.dark;
+  return (
+    <header style={{
+      padding: '10px',
+      background: `linear-gradient(
             180deg
             , ${theme.gradientOne} 0%, ${theme.gradientTwo} 100%`
-        }}>
-            <div id="header">
-                <Div isLightTheme={isLightTheme} theme={theme}>&lt;div&gt;</Div>
-                <Lucy theme={theme}>I am Lucy.</Lucy>
-                <Title theme={theme}>A full stack web developer based in sunny Barcelona</Title>
-                <LanguageList theme={theme}>I speak JavaScript, React.js, MongoDB, Mongoose, Express.js, Node.js, HTML5, CSS3, English, Italian and Spanish. When I'm not coding, I'm probaby dreaming about it. :&#41;</LanguageList>
-            </div>
-            <LanguageTabs>
-                <Languages isLightTheme={isLightTheme} theme={theme}><h4>JavaScript</h4></Languages>
-                <Languages isLightTheme={isLightTheme} theme={theme}><h4>React</h4></Languages>
-                <Languages isLightTheme={isLightTheme} theme={theme}><h4>MongoDB</h4></Languages>
-                <Languages isLightTheme={isLightTheme} theme={theme}><h4>Mongoose</h4></Languages>
-                <Languages isLightTheme={isLightTheme} theme={theme}><h4>Express.js</h4></Languages>
-                <Languages isLightTheme={isLightTheme} theme={theme}><h4>Node.js</h4></Languages>
-                <Languages isLightTheme={isLightTheme} theme={theme}><h4>HTML5</h4></Languages>
-                <Languages isLightTheme={isLightTheme} theme={theme}><h4>CSS3</h4></Languages>
-                <Languages isLightTheme={isLightTheme} theme={theme}><h4>Bootstrap</h4></Languages>
-            </LanguageTabs>
-            <a href="#about">
-                <img src={ArrowDown} alt="" />
-            </a>
-        </header>
-    );
+    }}>
+      <div id="header">
+        <Div isLightTheme={isLightTheme} theme={theme}>&lt;div&gt;</Div>
+        <Lucy theme={theme}>I am Lucy.</Lucy>
+        <Title theme={theme}>A full stack web developer based in sunny Barcelona</Title>
+        <LanguageList theme={theme}>I speak JavaScript, React.js, MongoDB, Mongoose, Express.js, Node.js, HTML5, CSS3, English, Italian and Spanish. When I'm not coding, I'm probaby dreaming about it. :&#41;</LanguageList>
+      </div>
+      <LanguageTabs>
+        <Languages isLightTheme={isLightTheme} theme={theme}><h4>JavaScript</h4></Languages>
+        <Languages isLightTheme={isLightTheme} theme={theme}><h4>React</h4></Languages>
+        <Languages isLightTheme={isLightTheme} theme={theme}><h4>MongoDB</h4></Languages>
+        <Languages isLightTheme={isLightTheme} theme={theme}><h4>Mongoose</h4></Languages>
+        <Languages isLightTheme={isLightTheme} theme={theme}><h4>Express.js</h4></Languages>
+        <Languages isLightTheme={isLightTheme} theme={theme}><h4>Node.js</h4></Languages>
+        <Languages isLightTheme={isLightTheme} theme={theme}><h4>HTML5</h4></Languages>
+        <Languages isLightTheme={isLightTheme} theme={theme}><h4>CSS3</h4></Languages>
+        <Languages isLightTheme={isLightTheme} theme={theme}><h4>Bootstrap</h4></Languages>
+      </LanguageTabs>
+      <a href="#about">
+        <img src={ArrowDown} alt="" />
+      </a>
+    </header>
+  );
 }
 
 export default Header;
