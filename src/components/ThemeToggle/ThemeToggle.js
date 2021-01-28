0d0151ec8fba6afcb3961d590d9b51b21a6dadbd
Lucy Quinn <lucy.quinn.uk@gmail.com>
2021-01-28 12:03:28 +0100

    add functionality for toggle light and dark mode logos.

--- src/components/ThemeToggle/ThemeToggle.js
+++ src/components/ThemeToggle/ThemeToggle.js
@@ -1,12 +1,18 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from '../../contexts/ThemeContext';
-import LightBulb from './../../images/button-light-bulb.svg';
+import LightBulbLight from './../../images/light-bulb-light.svg';
+import LightBulbDark from './../../images/light-bulb-dark.svg';
+
 
 const ThemeToggle = () => {
-    const { toggleTheme } = useContext(ThemeContext);
+    const { toggleTheme, isLightTheme } = useContext(ThemeContext);
     return (
         <div onClick={toggleTheme}>
-            <img src={LightBulb} alt="icon of light bulb" />
+            {isLightTheme ?
+                <img src={LightBulbLight} alt="icon of light bulb" />
+                :
+                <img src={LightBulbDark} alt="icon of dark bulb" />
+            }
         </div>
     );
 }
