73f3fd0481747a93dfc09f2594f4c067c515627f
Lucy Quinn <lucy.quinn.uk@gmail.com>
2021-01-28 14:35:41 +0100

    add styled components and functionality for dark and light theme.

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -1,29 +1,61 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../../contexts/ThemeContext';
+import styled from 'styled-components';
+
+
+//Styled components
+
+const Container = styled.div`
+  padding: 5px;
+  background: ${({ isLightTheme, theme }) => isLightTheme ? null : theme.background};
+
+`
+const Div = styled.p`
+  color: ${({ isLightTheme, theme }) => isLightTheme ? theme.primaryColor : theme.div};
+  margin: 0;
+`
+const Heading = styled.h2`
+  color: ${({ theme }) => theme.primaryColor};
+`
+const Experience = styled.div`
+
+`
+const School = styled.p`
+  color: ${({ isLightTheme, theme }) => isLightTheme ? theme.primaryColor : theme.div};
+
+`
+
+const Qualification = styled.h4`
+  color: ${({ theme }) => theme.bodyCopy};
+`
+
 
 const Education = () => {
+    const { isLightTheme, themes } = useContext(ThemeContext);
+    const theme = isLightTheme ? themes.light : themes.dark;
     return (
-        <div id="education">
-            <p>&lt;div&gt;</p>
-            <h2>Education</h2>
-            <div>
-                <p>Ironhack: Oct 2020 - Jan 2021 (Spain)</p>
-                <h4>Full Stack Web Development Graduate</h4>
-            </div>
-            <div>
-                <p>Don Milani: Oct 2012 - Dec 2012 (Italy)</p>
-                <h4>CIL Duo (B2 Higher Intermediate Italian)</h4>
-            </div>
-            <div>
-                <p>Leeds University: Sept 2008 - Jul 2012</p>
-                <h4>Spanish & Latin American Studies (BA Hons)</h4>
-                <h4>Web Development Module (JavaScript, HTML, CSS)</h4>
-                <h4>Erasmus Year (Oviedo, Spain): Sept 2010 - Jul 2011</h4>
-            </div>
-            <div>
-                <p>Cardiff Metropolitan University: Sept 2006 - Jul 2007</p>
-                <h4>Art & Design Foundatio</h4>
-            </div>
-        </div>
+        <Container isLightTheme={isLightTheme} theme={theme} id="education">
+            <Div isLightTheme={isLightTheme} theme={theme}>&lt;div&gt;</Div>
+            <Heading theme={theme}>Education</Heading>
+            <Experience>
+                <School isLightTheme={isLightTheme} theme={theme}>Ironhack: Oct 2020 - Jan 2021 (Spain)</School>
+                <Qualification theme={theme}>Full Stack Web Development Graduate</Qualification>
+            </Experience>
+            <Experience>
+                <School isLightTheme={isLightTheme} theme={theme}>Don Milani: Oct 2012 - Dec 2012 (Italy)</School>
+                <Qualification theme={theme}>CIL Duo (B2 Higher Intermediate Italian)</Qualification>
+            </Experience>
+            <Experience>
+                <School isLightTheme={isLightTheme} theme={theme}>Leeds University: Sept 2008 - Jul 2012</School>
+                <Qualification theme={theme}>Spanish & Latin American Studies (BA Hons)</Qualification>
+                <Qualification theme={theme}>Web Development Module (JavaScript, HTML, CSS)</Qualification>
+                <Qualification theme={theme}>Erasmus Year (Oviedo, Spain): Sept 2010 - Jul 2011</Qualification>
+            </Experience>
+            <Experience>
+                <School isLightTheme={isLightTheme} theme={theme}>Cardiff Metropolitan University: Sept 2006 - Jul 2007</School>
+                <Qualification theme={theme}>Art & Design Foundatio</Qualification>
+            </Experience>
+        </Container>
     );
 }
 
