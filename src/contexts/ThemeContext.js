1bb2ab7d320736b05a36a33e3809087a0e6be28f
Lucy Quinn <lucy.quinn.uk@gmail.com>
2021-01-28 12:06:06 +0100

    change properties of state.

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -4,7 +4,7 @@ const ThemeContextProvider = (props) => {
     const [isLightTheme, setLightTheme] = useState(true);
     const [themes, setThemes] = useState({
         light: { primaryColor: '#493d6e', div: '#cec8ff', bodyCopy: '#818181', gradientOne: '#91CCFF', gradientTwo: '#CEC8FF' },
-        dark: { primaryColor: '#FFFFFF', div: '#9fcaff', bodyCopy: '#c6c1cf', gradientOne: '#463E84', gradientTwo: '#6653B7', background: '#181227' }
+        dark: { primaryColor: '#FFFFFF', div: '#9fcaff', bodyCopy: '#c6c1cf', gradientOne: '#7576E8', gradientTwo: '#463E84', background: '#181227' }
     });
 
     const toggleTheme = () => {
