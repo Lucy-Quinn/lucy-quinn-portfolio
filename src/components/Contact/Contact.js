a4f1445aab748cf905112c86edb99e622b634710
Lucy Quinn <lucy.quinn.uk@gmail.com>
2021-01-28 16:26:59 +0100

    add styled components and functionality for dark and light theme. Change icons for social media and arrow up for dark and light mode.

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,31 +1,65 @@
-import React from 'react';
+import React, { useContext } from 'react';
 // import { HashLink as Link } from 'react-router-hash-link';
 import styled from 'styled-components';
 import './Contact.css';
+import { ThemeContext } from '../../contexts/ThemeContext';
+import ArrowUpLight from './../../images/button-arrow-up-light.svg';
+import ArrowUpDark from './../../images/button-arrow-down-dark.svg';
 
-const Icon = styled.a`
-    /* font-size: ${props => props.arrow ? '3rem' : '4rem'}; */
-    font-size: 4rem;
-    padding: 10px;
-    /* &:visited,&:link { color: ${props => props.primary && 'red'}} */
-`
-const Arrow = styled.p`
-    font-size: 1.6rem;
-`
-const ArrowBorder = styled.div`
-  height: 54px;
-  width: 56px;
-  border: 1px solid black;
-  border-radius: 50%;
-  margin: 0 auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
+import SkypeLight from './../../images/skype-light.svg';
+import SkypeDark from './../../images/skype-dark.svg';
+import LinkedinLight from './../../images/linkedin-light.svg';
+import LinkedinDark from './../../images/linkedin-dark.svg';
+import GithubLight from './../../images/github-light.svg';
+import GithubDark from './../../images/github-dark.svg';
 
 
+
+//Styled components
+
+const Icons = styled.div`
+    display: flex;
+    justify-content: space-around;
+    margin: 25px 0 20px 0;
+`
+
+const ArrowUp = styled.img`
+`
+
+const Div = styled.p`
+  color: ${({ isLightTheme, theme }) => isLightTheme ? theme.primaryColor : theme.div};
+  margin: 50px 0 0 0;
+`
+
+const Heading = styled.h2`
+  color: ${({ theme }) => theme.primaryColor};
+`
+const Number = styled.h2`
+  color: ${({ theme }) => theme.primaryColor};
+`
+
+const Tie = styled.p`
+  color: ${({ theme }) => theme.primaryColor};
+`
+
+const Socials = styled.p`
+  color: ${({ theme }) => theme.primaryColor};
+  margin: 50px 0 0 0;
+`
+
+const Top = styled.p`
+  color: ${({ theme }) => theme.primaryColor};
+  margin: 0 0 75px 0;
+  font-weight: 300;
+  & > h4 {
+        font-weight: 300;
+        margin: 5px 0 0 0;
+    }
+`
 
 const Contact = () => {
+    const { isLightTheme, themes } = useContext(ThemeContext);
+    const theme = isLightTheme ? themes.light : themes.dark;
 
     //Create a link so it is formatted to send an email
     const Mailto = ({ email, subject = '', body = '', children }) => {
@@ -36,43 +70,65 @@ const Contact = () => {
     };
 
     return (
-        <div id="contact">
+        <div style={{
+            padding: '10px',
+            background: `linear-gradient(
+                  180deg
+                  , ${theme.gradientTwo} 0%, ${theme.gradientOne} 100%`
+        }} id="contact">
             <div>
-                <p>&lt;div&gt;</p>
-                <h2>Contact Lucy</h2>
-                <h3>+44(0)7894 274 470</h3>
-                <h3>+34 634 328 672</h3>
-                <h5>Spanish permanent residency (TIE)</h5>
-                <h5>Follow me on socials and GitHub</h5>
+                <Div isLightTheme={isLightTheme} theme={theme}>&lt;div&gt;</Div>
+                <Heading theme={theme}>Contact Lucy</Heading>
+                <Number theme={theme}>+44(0)7894 274 470</Number>
+                <Number theme={theme}>+34 634 328 672</Number>
+                <Tie theme={theme}>Spanish permanent residency (TIE)</Tie>
+                <Socials theme={theme}>Follow me on socials and GitHub</Socials>
             </div>
 
             <div>
-                <Icon target="_blank" href="https://www.linkedin.com/in/lucy-quinn/">
-                    <i class="fab fa-linkedin"></i>
-                </Icon>
-                <Icon target="_blank" href="https://github.com/Lucy-Quinn">
-                    <i class="fab fa-github"></i>
-                </Icon>
-                <Icon target="_blank" href="https://www.linkedin.com/in/lucy-quinn/">
-                    <i class="fab fa-skype"></i>
-                </Icon>
-                <Mailto email="lucy.quinn.uk@gmail.com" subject="Let's Talk" body="Hello world!">
-                    <i class="fas fa-envelope"></i>
-                </Mailto>
+                {isLightTheme ?
+                    (
+                        <Icons>
+                            <a target="_blank" href="https://www.linkedin.com/in/lucy-quinn/">
+                                <img src={LinkedinLight} alt="a> of linkedin" />
+                            </a>
+                            <a target="_blank" href="https://github.com/Lucy-Quinn">
+                                <img src={GithubLight} alt="a> of github" />
+                            </a>
+                            <a target="_blank" href="https://www.linkedin.com/in/lucy-quinn/">
+                                <img src={SkypeLight} alt="a> of skype" />
+
+                            </a>
+                        </Icons>
+                    ) :
+                    (
+                        <Icons>
+
+                            <a target="_blank" href="https://www.linkedin.com/in/lucy-quinn/">
+                                <img src={LinkedinDark} alt="a> of linkedin" />
+                            </a>
+                            <a target="_blank" href="https://github.com/Lucy-Quinn">
+                                <img src={GithubDark} alt="a> of github" />
+                            </a>
+                            <a target="_blank" href="https://www.linkedin.com/in/lucy-quinn/">
+                                <img src={SkypeDark} alt="a> of skype" />
+                            </a>
+                        </Icons>
+                    )
+                }
+
             </div>
             <div>
-                <ArrowBorder>
-                    <a href="#header">
-                        <div className="arrow-up">
-                            <Arrow>
-                                &#8593;
-                            </Arrow>
-                        </div>
-                    </a>
-                </ArrowBorder>
                 <a href="#header">
-                    <h5>Back to top</h5>
+                    {isLightTheme ?
+                        <ArrowUp src={ArrowUpLight} alt="a> of arrow pointing up"></ArrowUp>
+                        :
+                        <ArrowUp src={ArrowUpDark} alt="a> of arrow pointing up"></ArrowUp>
+                    }
                 </a>
+                <Top theme={theme} href="#header">
+                    <h4>Back to top</h4>
+                </Top>
             </div>
         </div>
 
