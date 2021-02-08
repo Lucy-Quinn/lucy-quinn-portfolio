c23b833bdbc7fccf4a6d0958f4ac51565dc5f1e4
Lucy Quinn <lucy.quinn.uk@gmail.com>
2021-02-08 15:31:32 +0100

    add content to description of portfolio and three dot effect.

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -24,8 +24,26 @@ const Description = styled.p`
   color: ${({ theme }) => theme.bodyCopy};
   padding-bottom: 30px;
 `
-
-
+const Dots = styled.span`
+  &::after {
+    display: inline-block;
+    animation: ellipsis 1.25s infinite;
+    content: ".";
+    width: 1em;
+    text-align: left;
+  }
+  @keyframes ellipsis {
+    0% {
+      content: ".";
+    }
+    33% {
+      content: "..";
+    }
+    66% {
+      content: "...";
+    }
+  }
+`
 const Portfolio = () => {
 
   const { isLightTheme, themes } = useContext(ThemeContext);
@@ -33,14 +51,13 @@ const Portfolio = () => {
 
   return (
     <Container isLightTheme={isLightTheme} theme={theme} id="portfolio">
-
       <Div isLightTheme={isLightTheme} theme={theme}>&lt;div&gt;</Div>
       <Heading theme={theme}>My Portfolio</Heading>
-      <Description theme={theme}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit neque, nobis illum quibusdam in sit odit asperiores aliquam sed aut, placeat magni repellendus non, vero dolores. Enim nemo neque quidem.</Description>
+      <Description theme={theme}>Here are a collection of my projects that I have created, where I have implemented the languages that I have learnt. There are more projects that I am working on <Dots></Dots> </Description>
       <Carousel />
-
     </Container>
   );
 }
 
 export default Portfolio;
+
