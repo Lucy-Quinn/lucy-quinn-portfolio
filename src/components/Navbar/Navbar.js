3cef19690d4b418dac31bd547f4766c79eedf22f
Lucy Quinn <lucy.quinn.uk@gmail.com>
2021-02-23 17:28:03 +0100

    change version of react and delete .eslintcache. Add animatepresence to navbar and animation delay to background component.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,29 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { motion } from 'framer-motion';
-import { Nav, NavigationLinksMobile, NavigationLinksDesktop, NavLinkList, Link, Menu, ToggleButton, ToggleImage, ThemeToggleAndLogo, Background, NavNotActive, ThemeToggleAndCv } from './Navbar.styled';
+import React, { useState, useContext, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import {
+  Nav,
+  NavigationLinksMobile,
+  NavigationLinksDesktop,
+  NavLinkList,
+  Link,
+  Menu,
+  ToggleButton,
+  ToggleImage,
+  ThemeToggleAndLogo,
+  Background,
+  NavNotActive,
+  ThemeToggleAndCv,
+} from "./Navbar.styled";
 
-import ToggleButtonOpen from './../../images/button-open.svg';
-import ToggleButtonCloseLight from './../../images/button-close-light.svg';
-import ToggleButtonCloseDark from './../../images/button-close-dark.svg';
-import ThemeToggle from './../../components/ThemeToggle/ThemeToggle';
-import { ThemeContext } from '../../contexts/ThemeContext';
-import Resume from './../../images/open-cv.svg';
+import ToggleButtonOpen from "./../../images/button-open.svg";
+import ToggleButtonCloseLight from "./../../images/button-close-light.svg";
+import ToggleButtonCloseDark from "./../../images/button-close-dark.svg";
+import ThemeToggle from "./../../components/ThemeToggle/ThemeToggle";
+import { ThemeContext } from "../../contexts/ThemeContext";
+import Resume from "./../../images/open-cv.svg";
 
-import Logo from './../Logo/Logo';
-import './Navbar.css'
-
+import Logo from "./../Logo/Logo";
+import "./Navbar.css";
 
 const toggleButtonVariants = {
   // hidden: {
@@ -19,9 +31,8 @@ const toggleButtonVariants = {
   // },
   // visible: {
   //   rotate: -360,
-
   // }
-}
+};
 const variantContainer = {
   // hidden: {
   //   opacity: 0,
@@ -32,24 +43,21 @@ const variantContainer = {
   //   when: "beforeChildren",
   //   // staggerChildren: 4,
   //   // delayChildren: 1,
-
   // }
-}
+};
 const variants = {
   // hidden: {
   //   opacity: 0,
   //   y: -400,
   //   // y: 0
-
   // },
   // visible: {
   //   opacity: 1,
   //   y: 0,
   //   // transition: { duration: 1, delay: 0.5 }
   //   transition: { duration: 1 }
-
   // }
-}
+};
 
 const Navbar = () => {
   const [isActive, setActive] = useState(false);
@@ -61,130 +69,157 @@ const Navbar = () => {
     let prevScrollpos = window.pageYOffset;
     window.onscroll = function () {
       let currentScrollPos = window.pageYOffset;
-      prevScrollpos < currentScrollPos && setTimeout(() => { setActive(false) }, 500);
+      prevScrollpos < currentScrollPos &&
+        setTimeout(() => {
+          setActive(false);
+        }, 500);
       prevScrollpos = currentScrollPos;
-    }
-  }, [])
+    };
+  }, []);
 
   const handleToggle = () => {
     setActive(!isActive);
-  }
+  };
 
   const screenWidth = window.innerWidth;
   return (
     <div id="navbar">
+      {isActive && (
+        <Background
+          className={`overlay${isActive ? "-active" : "-not-active"}`}
+          isActive={isActive}
+        />
+      )}
 
-      <Background className={isActive ? 'background full' : "not-active"} isActive={isActive} />
       <Nav theme={theme}>
-        {screenWidth <= 768 &&
+        {screenWidth <= 768 && (
           <>
             {/* If isActive is true show nav links and close toggle button */}
-            {isActive ?
-              <div style={{ width: '100%' }}>
-                <NavigationLinksMobile theme={theme} isLightTheme={isLightTheme}>
-                  <NavLinkList className={isActive ? "active" : "not-active"}>
-                    <Menu>
-                      <p style={{ color: theme.bodyCopy }}>Menu</p>
-                    </Menu>
-                    <Link theme={theme} href="#about" >
-                      <h2>About Lucy</h2>
-                    </Link>
-                    <Link theme={theme} href="#portfolio" >
-                      <h2>Portfolio</h2>
-                    </Link>
-                    <Link theme={theme} href="#education">
-                      <h2>Education</h2>
-                    </Link>
-                    <Link contactLink theme={theme} href="#contact">
-                      <h2>Contact</h2>
-                    </Link>
-                    <Link resumeLink theme={theme} rel="noopener noreferrer" target="_blank" href="https://drive.google.com/file/d/1nFLSELiQ6yCG8C1aW9e4CHg8dTh6vawa/view?usp=sharing">
-                      <img src={Resume} alt="icon to open resume" />
-                    </Link>
-                  </NavLinkList>
-                  {isLightTheme ?
-                    (<ToggleButton crossToggle onClick={handleToggle}>
-                      <ToggleImage src={ToggleButtonCloseLight} alt="toggle button close icon"
-                        style={isActive ? { animation: 'openNav 1s' } : { animation: 'closeNav 1s' }}
-                        crossToggle onClick={handleToggle}
-                      // animate={isActive ? "visible" : "hidden"}
-                      // variants={toggleButtonVariants}
-                      />
-                    </ToggleButton>)
-                    :
-                    (<ToggleButton crossToggle onClick={handleToggle}>
-                      <motion.img src={ToggleButtonCloseDark} alt="toggle button close icon"
-                        animate={isActive ? "visible" : "hidden"}
-                        variants={toggleButtonVariants}
+            <AnimatePresence>
+              {isActive && (
+                <motion.div
+                  style={{ width: "100%" }}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0, background: "red" }}
+                >
+                  <NavigationLinksMobile
+                    theme={theme}
+                    isLightTheme={isLightTheme}
+                  >
+                    <NavLinkList className={isActive ? "active" : "not-active"}>
+                      <Menu>
+                        <p style={{ color: theme.bodyCopy }}>Menu</p>
+                      </Menu>
+                      <Link theme={theme} href="#about">
+                        <h2>About Lucy</h2>
+                      </Link>
+                      <Link theme={theme} href="#portfolio">
+                        <h2>Portfolio</h2>
+                      </Link>
+                      <Link theme={theme} href="#education">
+                        <h2>Education</h2>
+                      </Link>
+                      <Link contactLink theme={theme} href="#contact">
+                        <h2>Contact</h2>
+                      </Link>
+                      <Link
+                        resumeLink
+                        theme={theme}
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href="https://drive.google.com/file/d/1nFLSELiQ6yCG8C1aW9e4CHg8dTh6vawa/view?usp=sharing"
+                      >
+                        <img src={Resume} alt="icon to open resume" />
+                      </Link>
+                    </NavLinkList>
+
+                    <ToggleButton crossToggle onClick={handleToggle}>
+                      <ToggleImage
+                        src={
+                          isLightTheme
+                            ? ToggleButtonCloseLight
+                            : ToggleButtonCloseDark
+                        }
+                        alt="toggle button close icon"
+                        isActive={isActive}
+                        crossToggle
+                        onClick={handleToggle}
                       />
                     </ToggleButton>
-                    )
-                  }
-                </NavigationLinksMobile>
-              </div>
-              :
-              //If isActive is false show open toggle button, themetoggle button and logo
-              (<NavNotActive>
-                <ThemeToggleAndLogo>
-                  <ThemeToggle />
-                  <Logo screenWidth={screenWidth} />
-                </ThemeToggleAndLogo>
-                <ToggleButton onClick={handleToggle}>
-                  <motion.img src={ToggleButtonOpen} alt="toggle button open icon"
-                    variants={toggleButtonVariants}
-                    animate={isActive ? "visible" : "hidden"}
-                  />
-                </ToggleButton>
-              </NavNotActive>)
-            }
+                  </NavigationLinksMobile>
+                </motion.div>
+              )}
+            </AnimatePresence>
+
+            <AnimatePresence>
+              {!isActive && (
+                <NavNotActive>
+                  <ThemeToggleAndLogo>
+                    <ThemeToggle />
+                    <Logo screenWidth={screenWidth} />
+                  </ThemeToggleAndLogo>
+                  <ToggleButton onClick={handleToggle}>
+                    <motion.img
+                      src={ToggleButtonOpen}
+                      alt="toggle button open icon"
+                      variants={toggleButtonVariants}
+                      animate={isActive ? "visible" : "hidden"}
+                    />
+                  </ToggleButton>
+                </NavNotActive>
+              )}
+            </AnimatePresence>
           </>
-        }
+        )}
 
+        {screenWidth > 768 && (
+          <>
+            <Logo screenWidth={screenWidth} />
 
-
-        {screenWidth > 768 &&
-          (
-            <>
-              <Logo screenWidth={screenWidth} />
-
-              <NavigationLinksDesktop theme={theme} isLightTheme={isLightTheme}
-              >
-                {/* style={isActive ? { display: 'flex', animation: 'expandnav 2s' } : { display: 'none' }} */}
-                <NavLinkList className={isActive ? "active" : "not-active"}
+            <NavigationLinksDesktop theme={theme} isLightTheme={isLightTheme}>
+              {/* style={isActive ? { display: 'flex', animation: 'expandnav 2s' } : { display: 'none' }} */}
+              <NavLinkList className={isActive ? "active" : "not-active"}>
+                <Menu>
+                  <p style={{ color: theme.div }}>Menu</p>
+                </Menu>
+                <Link
+                  variants={variants}
+                  theme={theme}
+                  href="#about"
+                  className={isActive ? "links" : "not-active"}
                 >
-                  <Menu >
-                    <p style={{ color: theme.div }}>Menu</p>
-                  </Menu>
-                  <Link variants={variants} theme={theme} href="#about" className={isActive ? "links" : "not-active"} >
-                    <h2>About Lucy</h2>
-                  </Link>
-                  <Link theme={theme} href="#portfolio">
-                    <h2>Portfolio</h2>
-                  </Link>
-                  <Link theme={theme} href="#education">
-                    <h2>Education</h2>
-                  </Link>
-                  <Link contactLink theme={theme} href="#contact">
-                    <h2>Contact</h2>
-                  </Link>
-
-                </NavLinkList>
-              </NavigationLinksDesktop>
-
-              <ThemeToggleAndCv>
-                <ThemeToggle />
-                <Link resumeLink theme={theme} rel="noopener noreferrer" target="_blank" href="https://drive.google.com/file/d/1nFLSELiQ6yCG8C1aW9e4CHg8dTh6vawa/view?usp=sharing">
-                  <img src={Resume} alt="icon to open resume" />
+                  <h2>About Lucy</h2>
                 </Link>
-              </ThemeToggleAndCv>
-            </>
-          )
-        }
+                <Link theme={theme} href="#portfolio">
+                  <h2>Portfolio</h2>
+                </Link>
+                <Link theme={theme} href="#education">
+                  <h2>Education</h2>
+                </Link>
+                <Link contactLink theme={theme} href="#contact">
+                  <h2>Contact</h2>
+                </Link>
+              </NavLinkList>
+            </NavigationLinksDesktop>
+
+            <ThemeToggleAndCv>
+              <ThemeToggle />
+              <Link
+                resumeLink
+                theme={theme}
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://drive.google.com/file/d/1nFLSELiQ6yCG8C1aW9e4CHg8dTh6vawa/view?usp=sharing"
+              >
+                <img src={Resume} alt="icon to open resume" />
+              </Link>
+            </ThemeToggleAndCv>
+          </>
+        )}
       </Nav>
     </div>
   );
-}
+};
 
 export default Navbar;
-
-
--- src/components/Navbar/Navbar.styled.js
+++ src/components/Navbar/Navbar.styled.js
@@ -1,5 +1,5 @@
-import styled from 'styled-components';
-import { motion } from 'framer-motion';
+import styled, { keyframes } from "styled-components";
+import { motion } from "framer-motion";
 
 const Nav = styled.nav`
   background: ${({ theme }) => theme.gradientOne};
@@ -11,7 +11,7 @@ const Nav = styled.nav`
   @media (min-width: 1024px) {
     flex-direction: row;
   }
-`
+`;
 
 const NavigationLinksMobile = styled(motion.div)`
   width: 100%;
@@ -20,34 +20,51 @@ const NavigationLinksMobile = styled(motion.div)`
   overflow-x: hidden;
   z-index: 1;
   top: 0;
-  background: ${({ isLightTheme, theme }) => isLightTheme ? theme.div : theme.languages};
-  border-radius: 0 0 50% 50%  / 70px;
-  .active{ 
-    display: flex;  
+  background: ${({ isLightTheme, theme }) =>
+    isLightTheme ? theme.div : theme.languages};
+  border-radius: 0 0 50% 50% / 70px;
+  .active {
+    display: flex;
     -webkit-animation: expandNav 1s alternate ease-in-out; /* Safari 4+ */
-    -moz-animation:    expandNav 1s alternate ease-in-out; /* Fx 5+ */
-    -o-animation:      expandNav 1s alternate ease-in-out; /* Opera 12+ */
-    animation:         expandNav 1s alternate ease-in-out; /* IE 10+, Fx 29+ */
+    -moz-animation: expandNav 1s alternate ease-in-out; /* Fx 5+ */
+    -o-animation: expandNav 1s alternate ease-in-out; /* Opera 12+ */
+    animation: expandNav 1s alternate ease-in-out; /* IE 10+, Fx 29+ */
     /* animation: expandNav 1s alternate ease-in-out; */
-  };
+  }
   @-webkit-keyframes expandNav {
-    0% { max-height: 0px; }
-      100% { max-height: 400px; }
+    0% {
+      max-height: 0px;
+    }
+    100% {
+      max-height: 400px;
+    }
   }
   @-moz-keyframes expandNav {
-    0% { max-height: 0px; }
-      100% { max-height: 400px; }
+    0% {
+      max-height: 0px;
+    }
+    100% {
+      max-height: 400px;
+    }
   }
   @-o-keyframes expandNav {
-    0% { max-height: 0px; }
-      100% { max-height: 400px; }
+    0% {
+      max-height: 0px;
+    }
+    100% {
+      max-height: 400px;
+    }
   }
   @keyframes expandNav {
-    0% { max-height: 0px; }
-      100% { max-height: 400px; }
+    0% {
+      max-height: 0px;
+    }
+    100% {
+      max-height: 400px;
+    }
   }
-  
-/* .not-active{
+
+  /* .not-active{
   animation: shrinkNav 1s alternate-reverse;
   };
 @keyframes shrinkNav {
@@ -61,13 +78,14 @@ const NavigationLinksMobile = styled(motion.div)`
     border-radius: 0;
     background: ${({ theme }) => theme.gradientOne};
   }
-`
+`;
 
 const NavigationLinksDesktop = styled(motion.div)`
   width: 100%;
   margin: 0;
-  background: ${({ isLightTheme, theme }) => isLightTheme ? '#fff' : theme.languages};
-  border-radius: 0 0 50% 50%  / 70px;
+  background: ${({ isLightTheme, theme }) =>
+    isLightTheme ? "#fff" : theme.languages};
+  border-radius: 0 0 50% 50% / 70px;
   @media (min-width: 1024px) {
     display: flex;
     opacity: 1;
@@ -75,13 +93,13 @@ const NavigationLinksDesktop = styled(motion.div)`
     border-radius: 0;
     background: ${({ theme }) => theme.gradientOne};
   }
-`
+`;
 
 const NavLinkList = styled(motion.ul)`
-flex-direction: column;
-width: 100%;
+  flex-direction: column;
+  width: 100%;
   margin: 0;
-border-radius: 0 0 50% 50%  / 70px;
+  border-radius: 0 0 50% 50% / 70px;
   padding: 0;
   display: none;
   @media (min-width: 1024px) {
@@ -91,113 +109,114 @@ border-radius: 0 0 50% 50%  / 70px;
     align-items: center;
     padding: 0;
     width: 100%;
-    
   }
-`
+`;
 
 const Link = styled(motion.a)`
   display: block;
   text-align: center;
   & > h2 {
     color: ${({ theme }) => theme.primaryColor};
-     font-weight: 300;
-     margin: 24px 0 0 0;
+    font-weight: 300;
+    margin: 24px 0 0 0;
   }
-  margin-bottom: ${props => props.contactLink && '36px'};
-  margin-bottom: ${props => props.resumeLink && '36px'};
+  margin-bottom: ${(props) => props.contactLink && "36px"};
+  margin-bottom: ${(props) => props.resumeLink && "36px"};
   @media (min-width: 1024px) {
-    margin-bottom: ${props => props.contactLink && '0'};
-  margin-bottom: ${props => props.resumeLink && '0'};
-  & > h2{
+    margin-bottom: ${(props) => props.contactLink && "0"};
+    margin-bottom: ${(props) => props.resumeLink && "0"};
+    & > h2 {
       font-size: 1rem;
-     margin: 0 0 0;
-
+      margin: 0 0 0;
     }
   }
-`
+`;
 
 const Menu = styled(motion.li)`
- text-decoration: none;
-.navbar-links li {
-  list-style: none;
-}
-& > p{
-  margin: 16px 0 0 0;
-}
-@media (min-width: 1024px) {
-    display: none;
-    
+  text-decoration: none;
+  .navbar-links li {
+    list-style: none;
   }
-`
+  & > p {
+    margin: 16px 0 0 0;
+  }
+  @media (min-width: 1024px) {
+    display: none;
+  }
+`;
 
 const ToggleButton = styled(motion.div)`
   position: absolute;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  postion: ${(props) => (props.crossToggle && 'absolute')};
-  top: ${(props) => (props.crossToggle ? '0.7rem' : '0.2rem')};
-  right: ${(props) => (props.crossToggle ? '0.7rem' : '0.2rem')};
-  z-index: 1;
+  postion: ${(props) => props.crossToggle && "absolute"};
+  top: ${(props) => (props.crossToggle ? "0.7rem" : "0.2rem")};
+  right: ${(props) => (props.crossToggle ? "0.7rem" : "0.2rem")};
   @media (min-width: 1024px) {
     display: none;
   }
-`
+`;
+
+const openNav = keyframes`
+    0% {
+    transform: rotate(0deg);
+    }
+    100% {
+    transform: rotate(90deg);
+    }
+`;
+
+const closeNav = keyframes`
+    0% {
+        transform: rotate(90deg);
+    }
+    100% {
+        transform: rotate(0deg);
+    }
+`;
+
+
 
 const ToggleImage = styled(motion.img)`
-/* closetoggle */
-/* .not-active{
-  animation: shrinkNav 1s alternate-reverse;
-  }; */
-
-  @-webkit-keyframes openNav {
-    0% { transform: rotate(0deg)}
-  100% { transform: rotate(90deg) }
-}
-@-moz-keyframes openNav {
-    0% { transform: rotate(0deg)}
-  100% { transform: rotate(90deg) }
-}
-@-o-keyframes openNav {
-    0% { transform: rotate(0deg)}
-  100% { transform: rotate(90deg) }
-}
-@keyframes openNav {
-    0% { transform: rotate(0deg)}
-  100% { transform: rotate(90deg) }
-}
-
-
-@keyframes closeNav {
-  0% { transform: rotate(90deg)}
-  100% { transform: rotate(0deg) }
-};
-
-`
+  animation: 1s ${({ isActive }) => isActive ? 'openNav' : 'closeNav'};
+`;
 
 const ThemeToggleAndLogo = styled.div`
-display: flex;
+  display: flex;
   justify-content: space-between;
   width: 69%;
   align-items: center;
   margin: 5px 94px 0 0;
   @media (min-width: 375px) {
     width: 66%;
-
   }
   @media (min-width: 425px) {
     width: 65%;
-
   }
   @media (min-width: 728px) {
     width: 58%;
-
   }
+`;
 
-`
+const showBackground = keyframes`
+    0% {   opacity: 0; }
+    100% { opacity: 1; }
+`;
+
+// opacity: ${({ isActive }) => isActive ? 1 : 0 };
+//   transition: opacity 5s;
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
 
 const Background = styled.div`
-opacity: 0;
   top: 0;
   left: 0;
   right: 0;
@@ -205,53 +224,25 @@ opacity: 0;
   background: rgba(0, 0, 0, 0.9);
   will-change: opacity;
   z-index: ${({ isActive }) => isActive && 1};
-  position: ${({ isActive }) => isActive && 'fixed'};
-  animation-delay: 2s;
-  /* @-webkit-keyframes showBackground {
-  0%   { opacity: 0; }
-  100% { opacity: 1; }
-}
-@-moz-keyframes showBackground {
-  0%   { opacity: 0; }
-  100% { opacity: 1; }
-}
-@-o-keyframes showBackground {
-  0%   { opacity: 0; }
-  100% { opacity: 1; }
-}
-@keyframes showBackground {
-  0%   { opacity: 0; }
-  100% { opacity: 1; }
-}
-.background{
-  opacity: 1;
-  background-color: red;
-  /* display: block; */
-  /* -webkit-animation: showBackground 1s; 
-  -moz-animation:    showBackground 1s; 
-  -o-animation:      showBackground 1s; 
-  animation:         showBackground 1s; */
-  /* animation-delay: 2s; */
-/* };  */
-
-
-`
+  position: ${({ isActive }) => isActive && "fixed"};
+  animation-delay: 1s;
+  animation: 1s ${fadeIn} ease-out;
+`;
 
 const NavNotActive = styled.div`
-display: flex;
-    justify-content: space-between;
-    width: 100%;
-`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
 
 // width 1028px
 const ThemeToggleAndCv = styled.div`
-@media (min-width: 1024px) {
-  display: flex;
+  @media (min-width: 1024px) {
+    display: flex;
     justify-content: center;
     align-items: center;
   }
-`
-
+`;
 
 export {
   Nav,
@@ -265,5 +256,5 @@ export {
   ThemeToggleAndLogo,
   Background,
   NavNotActive,
-  ThemeToggleAndCv
-}
+  ThemeToggleAndCv,
+};
